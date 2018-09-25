efcfdb89c3690eb4183f0bcdf077899cd3b7e8a3
Dillon Astin <astindillon@gmail.com>
2018-09-25 11:20:37 -0600

    added reverse complement function

--- ecoli_ori.py
+++ ecoli_ori.py
@@ -8,3 +8,19 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+def reverse_complement(input):
+    reverse = input[::-1]
+    complement = ""
+    for x in reverse:
+        if x == "A":
+            complement += "T"
+        if x == "T":
+            complement += "A"
+        if x == "C":
+            complement += "G"
+        if x == "G":
+            complement += "C"
+    return complement
